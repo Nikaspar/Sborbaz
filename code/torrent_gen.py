80daec16709bbdf4286a20fcbecbd09981714bfc
Nikaspar <87052209+Nikaspar@users.noreply.github.com>
2022-01-13 04:13:04 +0300

    создает торрент из baselist

--- code/torrent_gen.py
+++ code/torrent_gen.py
@@ -1,40 +1,50 @@
 import os
-from datetime import date
 import torf
+from datetime import date
+from shutil import copytree
 from torf import Torrent
-from zipfile import ZipFile
 
 
-def create_archive(array: set, source: str, upload_dir: str, zip_name: str):
-    zip_path = os.path.join(upload_dir, zip_name)
+def get_folders(array: set, source: str, destination: str, name: str):
+    for folder in array:
+        if os.path.exists(os.path.join(source, folder)):
+            print('Copy "{}" to "{}{}{}"'.format(folder, destination, os.sep, name))
+        try:
+            copytree(os.path.join(source, folder), os.path.join(destination, name, folder))
+        except FileNotFoundError:
+            print(f'[warn]Folder "{folder}" not fount. Continue\n')
+            continue
+        except FileExistsError:
+            print(f'[warn]Folder "{folder}" already exists. Continue\n')
+            continue
+        else:
+            print('Copy is done\n')
 
-    with ZipFile(f'{zip_path}.zip', 'a') as archive:
-        for folder in array:
-            try:
-                archive.write(os.path.join(source, folder))
-            except FileNotFoundError:
-                continue
 
-
-def create_torrent(archive_name: str, upload: str, torrents: str):
+def create_torrent(upload: str, name: str, torrents: str):
     try:
-        t = Torrent(path=os.path.join(upload, f'{archive_name}.zip'),
+        print('Torrent file is being created')
+        t = Torrent(path=os.path.join(upload, f'{name}'),
                     trackers=['udp://tracker.openbittorrent.com:80/announce',
                               'udp://tracker.opentrackr.org:1337/announce'])
 
         t.private = True
         t.generate(threads=8)
-        t.write(os.path.join(torrents, f'{archive_name}.torrent'))
-    except torf.TorfError:
+        t.write(os.path.join(torrents, f'{name}.torrent'))
+    except torf.TorfError as ex:
+        print(f'[warn] {ex}\n')
         pass
+    else:
+        print('Torrent file created\n')
+    print('All done')
 
 
-USERNAME = 'user'
-DIRS_ARRAY = ['ABC', 'DEF', 'a']
+USERNAME = input('username:\n>>> ')  # 'user'
+DIRS_ARRAY = open('baselist.txt').read().split()  # ['ABC', 'DEF', 'a']
 BASE_PATH = os.path.join('A:', 'Bases')
 UPLOAD_PATH = os.path.join('..', 'uploads')
-ARCHIVE_NAME = '{}_{}'.format(USERNAME, date.today())
+UPLOAD_NAME = '{}_{}'.format(USERNAME, date.today())
 TORRENTS_PATH = os.path.join('..', 'torrents')
 
-create_archive(set(DIRS_ARRAY), BASE_PATH, UPLOAD_PATH, ARCHIVE_NAME)
-create_torrent(ARCHIVE_NAME, UPLOAD_PATH, TORRENTS_PATH)
+get_folders(set(DIRS_ARRAY), BASE_PATH, UPLOAD_PATH, UPLOAD_NAME)
+create_torrent(UPLOAD_PATH, UPLOAD_NAME, TORRENTS_PATH)
